41a6c408d69d3882b139172469b5f08e74d7ce74
GvMazzon25 <gvmazzon25@gmail.com>
2021-10-25 17:43:57 +0200

    selection

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
 const father = document.querySelector('.father');
-let square = document.quarySelector('.square')
+let square = document.querySelector('.square')
 
 for (let index = 1; index <= 100; index++){
    father.innerHTML += `<div class="square square-${index}">${index}</div>`;
 
-   if((index==5) && (index%3==0)){
-     
+   if((index%5==0) && (index%3==0)){
+      //square.classList.add("square-both");
+   }else if(index%5==0){
+      //square.classList.add("square-five");
+   }
+   else if(index%3==0){
+      //square.classList.add("square-three");
    }
 }
