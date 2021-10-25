62a9f2bbbfb64aebf7e0f69f879ec7732c12aaff
GvMazzon25 <gvmazzon25@gmail.com>
2021-10-25 17:15:45 +0200

    problem

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 const father = document.querySelector('.father');
+let square = document.quarySelector('.square')
 
 for (let index = 1; index <= 100; index++){
    father.innerHTML += `<div class="square square-${index}">${index}</div>`;
+
+   if((index==5) && (index%3==0)){
+     
+   }
 }
