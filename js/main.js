c146bcbcc8e00bc27f80224eaea01cca985d071e
GvMazzon25 <gvmazzon25@gmail.com>
2021-10-25 16:05:10 +0200

    square-end

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 const father = document.querySelector('.father');
 
 for (let index = 1; index <= 100; index++){
-   father.innerHtml += `<div class="square square${index}">${index}</div>`;
-   console.log(index)
+   father.innerHTML += `<div class="square square-${index}">${index}</div>`;
 }
