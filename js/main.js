e40cedc5dc9fbd2805a1bf6a782993813b383d4c
GvMazzon25 <gvmazzon25@gmail.com>
2021-10-25 17:59:50 +0200

    console.log

--- js/main.js
+++ js/main.js
@@ -5,11 +5,11 @@ for (let index = 1; index <= 100; index++){
    father.innerHTML += `<div class="square square-${index}">${index}</div>`;
 
    if((index%5==0) && (index%3==0)){
-      //square.classList.add("square-both");
+     console.log(index)
    }else if(index%5==0){
-      //square.classList.add("square-five");
+      console.log(index)
    }
    else if(index%3==0){
-      //square.classList.add("square-three");
+      console.log(index)
    }
 }
