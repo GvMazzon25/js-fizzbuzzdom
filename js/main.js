f3c2735cf7a61d86d8cf56a60575851a28eddb49
GvMazzon25 <gvmazzon25@gmail.com>
2021-10-25 20:19:23 +0200

    no-solution

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
-const father = document.querySelector('.father');
-let square = document.querySelector('.square')
+const father = document.getElementById('father');
+const box = document.getElementsByClassName("box");
+console.log(box)
 
 for (let index = 1; index <= 100; index++){
-   father.innerHTML += `<div class="square square-${index}">${index}</div>`;
+   father.innerHTML += `<div class="box box-${index}">${index}</div>`;
 
    if((index%5==0) && (index%3==0)){
-     console.log(index)
+      console.log(index)
    }else if(index%5==0){
       console.log(index)
    }
